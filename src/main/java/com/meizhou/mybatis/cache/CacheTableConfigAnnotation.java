aea655e8a735a46cb3e6d0127b37fa2fce9ac5d7
meizhou <1311005416@qq.com>
2018-12-27 15:56:14 +0800

    update

---
+++ src/main/java/com/meizhou/mybatis/cache/CacheTableConfigAnnotation.java
@@ -0,0 +1,29 @@
+package com.meizhou.mybatis.cache;
+
+import java.lang.annotation.*;
+
+/**
+ * Created by meizhou on 2018/12/27.
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+public @interface CacheTableConfigAnnotation {
+
+    String prefix();
+
+    String tableName();
+
+    String[] cacheKeys();
+
+    int expireTime() default 7 * 3600 * 24;
+
+    boolean isCache() default true;
+
+    String cacheClient();
+
+    String cacheHandler() default "CommonCacheHandler";
+
+    String[] generatedKeys();
+
+}
